6ed727d37f89c4f731d8e1c60af58410b5ae7d44
greenman20130 <deadbread228@gmail.com>
2024-03-14 08:01:46 +0300

    Update serializers.py
    Added new serializer Level

--- pass/mount/serializers.py
+++ pass/mount/serializers.py
@@ -6,3 +6,9 @@ class UsersSerializer(serializers.ModelSerializer):
     class Meta:
         model = Users
         fields = ['email', 'last_name', 'first_name', 'middle_name', 'phone']
+    
+
+class LevelSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Level
+        fields = ['winter', 'spring', 'summer', 'autumn']
