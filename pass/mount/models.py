ee73a52cf5aed6b8a556251c514b06bfdf34ed8c
greenman20130 <deadbread228@gmail.com>
2024-03-14 07:06:00 +0300

    Update models.py
    added new model Pass
    use pep8

--- pass/mount/models.py
+++ pass/mount/models.py
@@ -1,9 +1,34 @@
 from django.db import models
 
 # Create your models here.
+
+
 class Users(models.Model):
-    email=models.EmailField(max_length=100)
-    last_name=models.CharField(max_length=100)
-    first_name=models.CharField(max_length=100)
-    middle_name=models.CharField(max_length=100)
-    phone=models.CharField(max_length=100)
+    email = models.EmailField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    first_name = models.CharField(max_length=100)
+    middle_name = models.CharField(max_length=100)
+    phone = models.CharField(max_length=100)
+
+
+class Pass(models.Model):
+    NEW = 'NW'
+    PENDING = 'PN'
+    ACCEPTED = 'AC'
+    REJECTED = 'RJ'
+    CHOICES = (
+        ('NW', 'new'),
+        ('PN', 'pending'),
+        ('AC', 'accepted'),
+        ('RJ', 'rejected'),
+    )
+
+    beauty_title = models.CharField(max_length=128)
+    title = models.CharField(max_length=128)
+    other_titles = models.CharField(max_length=128)
+    connect = models.CharField(max_length=128)
+    add_time = models.DateTimeField(auto_now_add=True)
+    coordinate_id = models.OneToOneField('Coords', on_delete=models.CASCADE)
+    tourist_id = models.ForeignKey(Users, on_delete=models.CASCADE)
+    status = models.CharField(max_length=2, choices=CHOICES, default=NEW)
+    level = models.ForeignKey('Level', on_delete=models.CASCADE)
