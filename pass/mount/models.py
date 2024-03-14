7af2e6541178b558f643cf00b299aa701dee5518
greenman20130 <deadbread228@gmail.com>
2024-03-14 06:59:55 +0300

    Update models.py
    Created new model "Users"
    With arguments: email, last_name, first_name.  middle_name and phone

--- pass/mount/models.py
+++ pass/mount/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Users(models.Model):
+    email=models.EmailField(max_length=100)
+    last_name=models.CharField(max_length=100)
+    first_name=models.CharField(max_length=100)
+    middle_name=models.CharField(max_length=100)
+    phone=models.CharField(max_length=100)
