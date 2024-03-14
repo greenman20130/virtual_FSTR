3715c469654867281c57a4694ddb3093eeb0967f
greenman20130 <deadbread228@gmail.com>
2024-03-14 08:03:34 +0300

    Update serializers.py
    Added new serializer Images

--- pass/mount/serializers.py
+++ pass/mount/serializers.py
@@ -2,13 +2,20 @@ from .models import *
 from drf_writable_nested import WritableNestedModelSerializer
 from rest_framework import serializers
 
+
 class UsersSerializer(serializers.ModelSerializer):
     class Meta:
         model = Users
         fields = ['email', 'last_name', 'first_name', 'middle_name', 'phone']
-    
+
 
 class LevelSerializer(serializers.ModelSerializer):
     class Meta:
         model = Level
         fields = ['winter', 'spring', 'summer', 'autumn']
+
+
+class ImagesSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Images
+        fields = ['image', 'title']
