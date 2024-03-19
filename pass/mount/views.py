d470eb8c625541cd08d49cf2b14042a2c81f32a3
greenman20130 <deadbread228@gmail.com>
2024-03-19 07:26:12 +0300

    Update views.py
    added method partical_update

--- pass/mount/views.py
+++ pass/mount/views.py
@@ -30,9 +30,11 @@ class PassViewset(viewsets.ModelViewSet):
     queryset = Pass.objects.all()
     serializer_class = PassSerializer
     filterset_fields = ('tourist_id__email',)
+    
 
     def create(selfself, request, *args, **kwargs):
         serializer = PassSerializer(data=request.data)
+
         if serializer.is_valid():
             serializer.save()
             return Response({
@@ -40,15 +42,44 @@ class PassViewset(viewsets.ModelViewSet):
                 'message': None,
                 'id': serializer.data['id'],
             })
+
         if status.HTTP_400_BAD_REQUEST:
             return Response({
                 'status': status.HTTP_400_BAD_REQUEST,
                 'message': 'Bad Request',
                 'id': None,
             })
+
         if status.HTTP_500_INTERNAL_SERVER_ERROR:
             return Response({
                 'status': status.HTTP_500_INTERNAL_SERVER_ERROR,
                 'message': 'Ошибка подключения к базе данных',
                 'id': None,
+            })
+        
+
+    def partial_update(self, request, *args, **kwargs):
+        pereval = self.get_object()
+
+        if pereval.status == 'new':
+            serializer = PassSerializer(
+                pereval, data=request.data, partial=True)
+
+            if serializer.is_valid():
+                serializer.save()
+                return Response({
+                    'state': '1',
+                    'message': 'Запись успешно изменена',
+                })
+
+            else:
+                return Response({
+                    'state': '0',
+                    'message': serializer.errors
+                })
+
+        else:
+            return Response({
+                'state': '0',
+                'message': f'Отклонено! Причина: {pereval.get_status_display()}'
             })
